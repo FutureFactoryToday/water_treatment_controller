dbaecf7014239d2cba72484ff81139a6e1699adf
Bogdanov Andrey <bogd-merz@yandex.ru>
2022-09-06 23:20:52 +0300

    New pictures added

--- GUI/Макет/Buttons/c/mod/wrenchButImgRect_modFile.c
+++ GUI/Макет/Buttons/c/mod/wrenchButImgRect_modFile.c
@@ -1,48 +1,4 @@
-#include <stdint.h>
-#include "TFT\lcd.h"
-#include "TFT\bmp.h"
-
-#define   WIDTH   140	
-#define   HEIGHT  140
-
-#if LCD_REVERSE16 == 0
-#define  RC(a)   a
-#endif
-#if LCD_REVERSE16 == 1
-#define  RC(a)   ((((a) & 0xFF) << 8) | (((a) & 0xFF00) >> 8))
-#endif
-
-// struct packing
-#ifdef  __GNUC__
-#pragma pack(push, 1)
-#elif   defined(__CC_ARM)	
-#pragma push
-#pragma pack(1)
-#endif
-
-const BITMAPSTRUCT gImage_WRENCHBUTRECT __attribute__((aligned)) =
-{
-  {
-    0x4d42u,
-    sizeof(BITMAPINFOHEADER) + sizeof(BITMAPFILEHEADER) + (WIDTH * HEIGHT * 2),
-    0x0000u,
-    0x0000u,
-    sizeof(BITMAPINFOHEADER) + sizeof(BITMAPFILEHEADER)
-  },
-  {
-    sizeof(BITMAPINFOHEADER),
-    WIDTH,
-    HEIGHT,
-    1u,
-    16,
-    0x00000003u,
-    (WIDTH * HEIGHT * 2),
-    0x00000000ul,
-    0x00000000ul,
-    0x00000000ul,
-    0x00000000ul
-  },
-  {RC(0XBDF7), 
+RC(0XBDF7), 
 RC(0XBDF7), 
 RC(0XBDF7), 
 RC(0XBDF7), 
@@ -17998,5 +17954,3 @@ RC(0XBDF7),
 RC(0XBDF7), 
 RC(0XBDF7), 
 RC(0XBDF7), 
-}
-};
