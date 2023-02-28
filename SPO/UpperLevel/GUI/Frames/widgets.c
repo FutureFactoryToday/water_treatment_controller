797969b652211e2983c13f77b2bfa1311358deac
Alexandr <i904gcc@gmail.com>
2023-02-28 10:33:37 +0300

    Lock colors in widgets

--- SPO/UpperLevel/GUI/Frames/widgets.c
+++ SPO/UpperLevel/GUI/Frames/widgets.c
@@ -1,8 +1,8 @@
 #include "widgets.h"
 
-button_t drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label, uint16_t color, bool isTouch)
+button_t drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label, bool isTouch)
 {
-    uint16_t radius = ySize/2;
+    uint16_t radius = ySize/4;
     
     button_t but;
     but.x = xPos;
@@ -21,8 +21,8 @@ button_t drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t 
     }
     else
     {
-        BSP_LCD_SetTextColor(color);
-        BSP_LCD_SetBackColor(color);
+        BSP_LCD_SetTextColor(LCD_COLOR_WHITEBLUE);
+        BSP_LCD_SetBackColor(LCD_COLOR_WHITEBLUE);
     }
 //    if(radius != 0)
 //    {
@@ -46,7 +46,7 @@ button_t drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t 
     return but;
 }
 
-button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label, uint16_t color1, uint16_t color2)
+button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label)
 {
     uint16_t radius = ySize/4;
     
@@ -63,7 +63,7 @@ button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t y
 //    
 //    if(radius != 0)
 //    {
-    BSP_LCD_SetTextColor(color1);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITEBLUE);
     BSP_LCD_FillRect(xPos, yPos + radius, xSize + 1, ySize - radius * 2);
     BSP_LCD_FillRect(xPos + radius, yPos, xSize - radius * 2, ySize + 1);
     
@@ -73,7 +73,7 @@ button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t y
     BSP_LCD_FillCircle((xPos + xSize) - radius, yPos + radius, radius);
    
     
-    BSP_LCD_SetTextColor(color2);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
     BSP_LCD_FillRect(xPos + 1, yPos + radius + 1, xSize - 1, ySize - radius * 2 - 2);
     BSP_LCD_FillRect(xPos + radius + 1, yPos + 1, xSize - radius * 2 - 2, ySize - 1);
     
@@ -86,8 +86,8 @@ button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t y
 //    {
 //        BSP_LCD_FillRect(xPos, yPos, xSize, ySize);
 //    }
-    BSP_LCD_SetBackColor(color2);
-    BSP_LCD_SetTextColor(color1);
+    BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
+    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
     BSP_LCD_DisplayStringAt(xPos + xSize/2, yPos + ySize/2 - 18, label, CENTER_MODE);
     
     return but;
