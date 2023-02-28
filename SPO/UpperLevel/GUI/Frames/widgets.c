224490f67b955bd7a2dfa0af2e28183a0e7d2370
Alexandr <i904gcc@gmail.com>
2023-02-28 10:07:31 +0300

    Modify widgets

--- SPO/UpperLevel/GUI/Frames/widgets.c
+++ SPO/UpperLevel/GUI/Frames/widgets.c
@@ -1,7 +1,19 @@
 #include "widgets.h"
 
-void drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t radius, uint16_t* label, uint16_t color, bool isTouch)
+button_t drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label, uint16_t color, bool isTouch)
 {
+    uint16_t radius = ySize/2;
+    
+    button_t but;
+    but.x = xPos;
+    but.y = yPos;
+    but.xSize = xSize;
+    but.ySize = ySize; 
+    but.isPressed = false;
+    but.wasPressed = false;
+    but.isReleased = false;
+    but.pressCnt = false;
+    
     if(isTouch)
     {
         BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
@@ -12,56 +24,73 @@ void drawFillButton (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySiz
         BSP_LCD_SetTextColor(color);
         BSP_LCD_SetBackColor(color);
     }
-    if(radius != 0)
-    {
-       BSP_LCD_FillRect(xPos, yPos + radius, xSize + 1, ySize - radius * 2);
-       BSP_LCD_FillRect(xPos + radius, yPos, xSize - radius * 2, ySize + 1);
+//    if(radius != 0)
+//    {
+    BSP_LCD_FillRect(xPos, yPos + radius, xSize + 1, ySize - radius * 2);
+    BSP_LCD_FillRect(xPos + radius, yPos, xSize - radius * 2, ySize + 1);
         
-       BSP_LCD_FillCircle(xPos + radius, yPos + radius, radius);
-       BSP_LCD_FillCircle(xPos + radius, (yPos + ySize) - radius, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius, (yPos + ySize) - radius, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius, yPos + radius, radius);
-    }
-    else
-    {
-        BSP_LCD_FillRect(xPos, yPos, xSize, ySize);
-    }
+    BSP_LCD_FillCircle(xPos + radius, yPos + radius, radius);
+    BSP_LCD_FillCircle(xPos + radius, (yPos + ySize) - radius, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius, (yPos + ySize) - radius, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius, yPos + radius, radius);
+//    }
+//    else
+//    {
+//        BSP_LCD_FillRect(xPos, yPos, xSize, ySize);
+//    }
     
     
     BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
     BSP_LCD_DisplayStringAt(xPos + xSize/2, yPos + ySize/2 - 18, label, CENTER_MODE);
+    
+    return but;
 }
 
-void drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t radius, uint16_t* label, uint16_t color1, uint16_t color2)
+button_t drawTextLabel (uint16_t xPos, uint16_t yPos, uint16_t xSize, uint16_t ySize, uint16_t* label, uint16_t color1, uint16_t color2)
 {
-    if(radius != 0)
-    {
-       BSP_LCD_SetTextColor(color1);
-       BSP_LCD_FillRect(xPos, yPos + radius, xSize + 1, ySize - radius * 2);
-       BSP_LCD_FillRect(xPos + radius, yPos, xSize - radius * 2, ySize + 1);
-        
-       BSP_LCD_FillCircle(xPos + radius, yPos + radius, radius);
-       BSP_LCD_FillCircle(xPos + radius, (yPos + ySize) - radius, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius, (yPos + ySize) - radius, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius, yPos + radius, radius);
-       
-        
-       BSP_LCD_SetTextColor(color2);
-       BSP_LCD_FillRect(xPos + 1, yPos + radius + 1, xSize - 1, ySize - radius * 2 - 2);
-       BSP_LCD_FillRect(xPos + radius + 1, yPos + 1, xSize - radius * 2 - 2, ySize - 1);
-        
-       BSP_LCD_FillCircle(xPos + radius + 1, yPos + radius + 1, radius);
-       BSP_LCD_FillCircle(xPos + radius + 1, (yPos + ySize) - radius - 1, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius - 1, (yPos + ySize) - radius - 1, radius);
-       BSP_LCD_FillCircle((xPos + xSize) - radius - 1, yPos + radius + 1, radius);
-    }
-    else
-    {
-        BSP_LCD_FillRect(xPos, yPos, xSize, ySize);
-    }
+    uint16_t radius = ySize/4;
+    
+    button_t but;
+    but.x = xPos;
+    but.y = yPos;
+    but.xSize = xSize;
+    but.ySize = ySize; 
+    but.isPressed = false;
+    but.wasPressed = false;
+    but.isReleased = false;
+    but.pressCnt = false;
+    
+//    
+//    if(radius != 0)
+//    {
+    BSP_LCD_SetTextColor(color1);
+    BSP_LCD_FillRect(xPos, yPos + radius, xSize + 1, ySize - radius * 2);
+    BSP_LCD_FillRect(xPos + radius, yPos, xSize - radius * 2, ySize + 1);
+    
+    BSP_LCD_FillCircle(xPos + radius, yPos + radius, radius);
+    BSP_LCD_FillCircle(xPos + radius, (yPos + ySize) - radius, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius, (yPos + ySize) - radius, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius, yPos + radius, radius);
+   
+    
+    BSP_LCD_SetTextColor(color2);
+    BSP_LCD_FillRect(xPos + 1, yPos + radius + 1, xSize - 1, ySize - radius * 2 - 2);
+    BSP_LCD_FillRect(xPos + radius + 1, yPos + 1, xSize - radius * 2 - 2, ySize - 1);
+    
+    BSP_LCD_FillCircle(xPos + radius + 1, yPos + radius + 1, radius);
+    BSP_LCD_FillCircle(xPos + radius + 1, (yPos + ySize) - radius - 1, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius - 1, (yPos + ySize) - radius - 1, radius);
+    BSP_LCD_FillCircle((xPos + xSize) - radius - 1, yPos + radius + 1, radius);
+//    }
+//    else
+//    {
+//        BSP_LCD_FillRect(xPos, yPos, xSize, ySize);
+//    }
     BSP_LCD_SetBackColor(color2);
     BSP_LCD_SetTextColor(color1);
     BSP_LCD_DisplayStringAt(xPos + xSize/2, yPos + ySize/2 - 18, label, CENTER_MODE);
+    
+    return but;
 }
 
 void drawMainBar(bool returnBut, uint16_t xPosLogo, uint16_t yPosLogo, uint16_t* label)
